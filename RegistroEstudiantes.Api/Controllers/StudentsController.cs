c6e1af9b7865b32a6e5c02571f32812159c20d10
Junior Valera <junior22ascinsero@gmail.com>
2026-06-09 23:59:47 -0400

    crear controlador inicial de estudiantes y Editar Program

--- RegistroEstudiantes.Api/Controllers/StudentsController.cs
+++ RegistroEstudiantes.Api/Controllers/StudentsController.cs
@@ -1,8 +1,56 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistroEstudiantes.Api.Models;
 
-public class Class1
+namespace RegistroEstudiantes.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentsController : ControllerBase
 {
-	public Class1()
-	{
-	}
+    private static readonly List<StudentDto> Students =
+    [
+        new StudentDto
+        {
+            Id = 1,
+            FirstName = "Junior",
+            LastName = "Valera",
+            StudentCode = "EST-001",
+            Email = "junior.valera@email.com",
+            Phone = "809-000-0001",
+            Career = "Ingeniería de Software",
+            BirthDate = new DateTime(2000, 8, 18),
+            Status = "Activo"
+        },
+        new StudentDto
+        {
+            Id = 2,
+            FirstName = "Yisel",
+            LastName = "Santana",
+            StudentCode = "EST-002",
+            Email = "yisel.santana@email.com",
+            Phone = "809-000-0002",
+            Career = "Ingeniería de Software",
+            BirthDate = new DateTime(1998, 1, 1),
+            Status = "Activo"
+        }
+    ];
+
+    [HttpGet]
+    public ActionResult<IEnumerable<StudentDto>> GetAll()
+    {
+        return Ok(Students);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<StudentDto> GetById(int id)
+    {
+        var student = Students.FirstOrDefault(student => student.Id == id);
+
+        if (student is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(student);
+    }
 }
