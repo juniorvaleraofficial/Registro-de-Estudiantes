77739e4ed23315adc519bc97e8f082925aeb359b
Junior Valera <junior22ascinsero@gmail.com>
2026-06-10 00:28:12 -0400

    crear servicio movil para consumir API de estudiantes

--- RegistroEstudiantes.Mobile/MauiProgram.cs
+++ RegistroEstudiantes.Mobile/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using RegistroEstudiantes.Mobile.Services;
 
 namespace RegistroEstudiantes.Mobile;
 
@@ -14,11 +15,11 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-
+        builder.Services.AddSingleton<StudentApiService>();
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
+        return builder.Build();
 	}
 }
