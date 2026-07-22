9d12e16ce5e7ea75fcbcc9f149da6dc8c39ab08e
Junior Valera <junior22ascinsero@gmail.com>
2026-07-22 01:05:13 -0400

    feat(calificaciones): conectar alertas y confirmación de eliminación

--- RegistroEstudiantes.Mobile/Views/CalificacionesPage.xaml.cs
+++ RegistroEstudiantes.Mobile/Views/CalificacionesPage.xaml.cs
@@ -13,7 +13,28 @@ public partial class CalificacionesPage : ContentPage
         viewModel = new CalificacionesViewModel();
         BindingContext = viewModel;
 
-        viewModel.SolicitarAlerta += async (titulo, mensaje, boton) =>
-            await DisplayAlertAsync(titulo, mensaje, boton);
+        viewModel.SolicitarAlerta += async (
+            titulo,
+            mensaje,
+            boton) =>
+        {
+            await DisplayAlertAsync(
+                titulo,
+                mensaje,
+                boton);
+        };
+
+        viewModel.SolicitarConfirmacion += async (
+            titulo,
+            mensaje,
+            botonAceptar,
+            botonCancelar) =>
+        {
+            return await DisplayAlertAsync(
+                titulo,
+                mensaje,
+                botonAceptar,
+                botonCancelar);
+        };
     }
 }
